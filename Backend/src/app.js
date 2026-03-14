e493295e9839f69c828c860d4ac64caaa8a55435
ManveerBains1000 <manveerjaat95@gmail.com>
2026-03-14 20:24:11 +0530

    feat: implement problem and test case management with CRUD operations

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-
+import problemRoutes from './routes/problem.route.js';
+import testCaseRoutes from './routes/testCase.route.js';
+app.use('/api',problemRoutes);
+app.use('/api',testCaseRoutes);
 export default app;
---
+++ Backend/src/models/submission.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const submissionSchema = new mongoose.Schema(
+    {
+        userId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true,
+        },
+        problemId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Problem",
+            required:true,
+        },
+        language:{
+            type:String,
+            required:true,
+        },
+        code:{
+            type:String,
+            required:true,
+        },
+        status:{
+            type:String,
+            enum:["Pending","Accepted","Wrong Answer","Runtime Error","Time Limit Exceeded"],
+            default:"Pending",
+        },
+        executionTimeMs:{
+            type:Number,
+        },
+        memoryUsedKb:{
+            type:Number,
+        },
+        failedTestCase:{
+            input:String,
+            expectedOutput:String,
+            actualOutput:String,
+        }
+
+    },
+    {
+        timestamps:true,
+    }
+);
+
+export const Submission = mongoose.model("Submission",submissionSchema);
